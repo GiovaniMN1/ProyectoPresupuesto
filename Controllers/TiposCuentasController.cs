1980979f9ed201eef17914eefaaafcf9d8c1967d
Giovani M <gioaldo172@gmail.com>
2024-05-05 00:24:25 -0600

    Inicio de Proyecto

---
+++ Controllers/TiposCuentasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+
+using System.Linq;
+using Dapper; // Importación de Dapper
+using System.Data.SqlClient;
+using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+        }
+
+        public IActionResult Crear()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            tipoCuenta.UsuarioId = 1;
+            repositorioTiposCuentas.Crear(tipoCuenta);
+            return View();
+        }
+    }
+}
